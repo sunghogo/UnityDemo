ec784edbe15b521fac1a086db9386996fe691676
sunghogo <70732249+sunghogo@users.noreply.github.com>
2024-12-03 18:24:22 -0500

    Demo Pacman2 update, bg collision 80% done

--- Demo/Assets/Movement.cs
+++ Demo/Assets/Movement.cs
@@ -3,18 +3,45 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed;
+    public Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed = 3F;
+        moveSpeed = 5F;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {
-            transform.Translate(Vector2.up * Time.deltaTime); // (0, 1)
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+            transform.Translate(Vector2.up * moveSpeed * Time.deltaTime); // (0, 1)
+        }
+        if (Input.GetKey(KeyCode.S ) || Input.GetKey(KeyCode.DownArrow)) {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", false);
+            transform.Translate(Vector2.down * moveSpeed * Time.deltaTime); // (0, 1)
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", true);
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime); // (0, 1)
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", true);
+            animator.SetBool("Right", false);
+            transform.Translate(Vector2.left * moveSpeed * Time.deltaTime); // (0, 1)
         }
     }
 }
