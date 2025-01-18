ad1b41fc616a990cd7a2aa86f900b996801e3e1d
Daniel <70732249+sunghogo@users.noreply.github.com>
2025-01-17 19:10:57 -0500

    Implemented queued movement for code along

--- Demo/Assets/Movement.cs
+++ Demo/Assets/Movement.cs
@@ -4,23 +4,26 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     public float moveSpeed;
-    public Vector2 direction; 
+    [SerializeField] private Vector2 _direction;
+    [SerializeField] private Vector2 _nextDirection; 
+
     public Animator animator;
 
     private SpriteRenderer _spriteRenderer;
     private Rigidbody2D _rigidbody;
-    private float _rayLength;
+    [SerializeField] private float _rayLength;
     private LayerMask _obstaclesLayer;
 
     // Start is called before the first frame update
     void Start()
     {
         moveSpeed = 5F;
-        direction = Vector2.zero;
+        _direction = Vector2.zero;
+        _nextDirection = Vector2.zero;
         animator = GetComponent<Animator>();
         _spriteRenderer = GetComponent<SpriteRenderer>();
         _rigidbody = GetComponent<Rigidbody2D>();
-        _rayLength = 1F;
+        _rayLength = 0.05F;
         _obstaclesLayer = LayerMask.GetMask("Obstacles");
 
     }
@@ -32,17 +35,23 @@ public class Movement : MonoBehaviour
         return Physics2D.BoxCast(origin, size, 0f, direction, _rayLength, _obstaclesLayer);
     }
 
+    private bool isColliding(Vector2 direction) {
+        RaycastHit2D hit = boxCast(direction);
+        if (hit.collider != null && hit.collider.CompareTag("Obstacle")) return true;
+        return false;
+    }
+
     void OnDrawGizmos() {
         if (_rigidbody == null || _spriteRenderer == null) return;
         Vector2 size = new Vector2(_spriteRenderer.bounds.size.x, _spriteRenderer.bounds.size.y);
         Vector2 origin = _rigidbody.position + size / 2;
-        Vector2 endPoint = origin + (direction * _rayLength);
+        Vector2 endPoint = origin + (_direction * _rayLength);
 
-        RaycastHit2D hit = boxCast(direction);
-        if (hit.collider != null && hit.collider.CompareTag("Obstacle"))
+        RaycastHit2D hit = boxCast(_direction);
+        if (isColliding(_direction))
         {
             Gizmos.color = Color.red;
-            Gizmos.DrawWireCube(origin + direction * hit.distance, size);
+            Gizmos.DrawWireCube(origin + _direction * hit.distance, size);
         }
         else
         {
@@ -52,39 +61,46 @@ public class Movement : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void FixedUpdate()
+    {        
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-            direction = Vector2.up;
+            _nextDirection = Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S ) || Input.GetKey(KeyCode.DownArrow)) {
+            _nextDirection = Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            _nextDirection = Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            _nextDirection = Vector2.left;
+        }
+
+        if (!isColliding(_nextDirection)) _direction = _nextDirection;
+        if (_direction == Vector2.up) {
             animator.SetBool("Up", true);
             animator.SetBool("Down", false);
             animator.SetBool("Left", false);
             animator.SetBool("Right", false);
-            transform.Translate(direction * moveSpeed * Time.deltaTime); // (0, 1)
         }
-        if (Input.GetKey(KeyCode.S ) || Input.GetKey(KeyCode.DownArrow)) {
-            direction = Vector2.down;
+        if (_direction == Vector2.down) {
             animator.SetBool("Up", false);
             animator.SetBool("Down", true);
             animator.SetBool("Left", false);
             animator.SetBool("Right", false);
-            transform.Translate(direction * moveSpeed * Time.deltaTime); // (0, 1)
         }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-            direction = Vector2.right;
-            animator.SetBool("Up", false);
-            animator.SetBool("Down", false);
-            animator.SetBool("Left", false);
-            animator.SetBool("Right", true);
-            transform.Translate(direction * moveSpeed * Time.deltaTime); // (0, 1)
-        }
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-            direction = Vector2.left;
+        if (_direction == Vector2.left) {
             animator.SetBool("Up", false);
             animator.SetBool("Down", false);
             animator.SetBool("Left", true);
             animator.SetBool("Right", false);
-            transform.Translate(direction * moveSpeed * Time.deltaTime); // (0, 1)
         }
+        if (_direction == Vector2.right) {
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Right", true);
+        }
+        if (!isColliding(_direction)) transform.Translate(_direction * moveSpeed * Time.deltaTime); // (0, 1)
     }
 }
